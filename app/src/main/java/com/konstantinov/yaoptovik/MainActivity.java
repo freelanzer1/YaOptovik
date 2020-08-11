04721b78ce84715841dd81a23345ff91b8a65733
Aleksandr Konstantinov <al_konst@bk.ru>
2020-08-11 13:55:04 +0500

    Добавлена русская локализация

--- app/src/main/java/com/konstantinov/yaoptovik/MainActivity.java
+++ app/src/main/java/com/konstantinov/yaoptovik/MainActivity.java
@@ -17,7 +17,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-//1111
+//11
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
